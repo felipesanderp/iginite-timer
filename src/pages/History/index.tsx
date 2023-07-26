f2706cfc8b1883dc5f64b0f4a2721d73e7a67ac3
felipesanderp <felipe.panisson@hotmail.com>
2023-07-26 12:15:29 -0300

    status indicator

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -20,28 +20,36 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Ha 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Ha 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Ha 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Ha 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
           </tbody>
         </table>
