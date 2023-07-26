7996e14a0f9c4ede6574dec1d0a38dd1253e45a5
felipesanderp <felipe.panisson@hotmail.com>
2023-07-26 11:12:08 -0300

    style inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,8 @@ import { Play } from 'phosphor-react'
 import {
   CountdownContainer,
   FormContainer,
+  TaskInput,
+  MinutesAmountInput,
   HomeContainer,
   Separator,
   StartCountdownButton,
@@ -14,10 +16,14 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
 
           <span>minutos.</span>
         </FormContainer>
